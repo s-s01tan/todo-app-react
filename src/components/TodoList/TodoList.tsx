8f70e8b7b6e08d19554c158ac0d162dc36770d29
s-s01tan <soltanlisoltan.js@gmail.com>
2023-02-24 15:15:53 +0400

    Pagination On Progress

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,10 +1,14 @@
-import { List } from "@mui/material";
+import { List, Pagination } from "@mui/material";
 import { Fragment } from "react";
 import Todo from "../Todo/Todo";
 import "./TodoList.css";
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   const { todos } = props;
+
+  const limit = 5;
+  const pageCount = Math.ceil(todos.length / limit);
+
   return (
     <Fragment>
       <List className="list-container">
@@ -13,6 +17,7 @@ const TodoList: React.FC<TodoListProps> = (props) => {
           .map((todo) => (
             <Todo todo={todo} key={todo.id} />
           ))}
+        <Pagination count={pageCount} color="primary" />
       </List>
     </Fragment>
   );
